4e63e16a565ed1370400c66339df15d204a74af6
KirylLapouski <lapkovskyk@mail.ru>
2018-09-30 11:08:50 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let res = 0;
+  for(let i=0;i<=number;i+=5){
+      res += multipleOfFiveTimes(i)
+  }
+  return res
 }
+
+function multipleOfFiveTimes(number){
+  let res = 0
+  if(!number)
+    return 0
+  while(!(number%5)){
+    res+=1;
+    number = number / 5
+  }
+  return res
+}
+
